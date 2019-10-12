24f2e3ca8e867fd5562196b928a6c3237cc52bf3
Xwilarg <xwilarg@yahoo.fr>
2019-10-12 21:57:20 +0900

    Add user command

--- MangasFan-Bot/CommunicationModule.cs
+++ MangasFan-Bot/CommunicationModule.cs
@@ -1,5 +1,10 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using DiscordUtils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace MangasFan_Bot
@@ -7,9 +12,49 @@ namespace MangasFan_Bot
     public class CommunicationModule : ModuleBase
     {
         [Command("Info")]
-        public async Task Info()
+        public async Task Info(params string[] _)
         {
             await ReplyAsync("", false, Utils.GetBotInfo(Program.P.StartTime, "MangasFan-Bot", Program.P.client.CurrentUser));
+        }
+
+        [Command("User")]
+        public async Task User(params string[] args)
+        {
+            IUser user = await Utils.GetUser(string.Join(" ", args), Context.Guild);
+            if (user == null)
+            {
+                await ReplyAsync("There is no user with this name");
+                return;
+            }
+            dynamic json;
+            using (HttpClient hc = new HttpClient())
+                json = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://www.mangasfan.fr/api_discord.php?name=" + Uri.EscapeDataString(user.Username) + "&id=" + user.Discriminator));
+            if (json == null)
+            {
+                await ReplyAsync("This user didn't link his Mangas'Fan and Discord account.");
+                return;
+            }
+            await ReplyAsync("", false, new EmbedBuilder
+            {
+                Title = json.username,
+                Description = json.description,
+                Color = Color.Blue,
+                Fields = new List<EmbedFieldBuilder>()
+                {
+                    new EmbedFieldBuilder
+                    {
+                        IsInline = true,
+                        Name = "Favorite Anime",
+                        Value = json.anime
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        IsInline = true,
+                        Name = "Favorite Manga",
+                        Value = json.manga
+                    }
+                }
+            }.Build());
         }
     }
 }
