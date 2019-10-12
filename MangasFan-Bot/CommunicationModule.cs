30f672305a9cb890cfc6c7105e09c0676ea463f8
Xwilarg <xwilarg@yahoo.fr>
2019-10-12 21:40:37 +0900

    Add empty Discord bot

---
+++ MangasFan-Bot/CommunicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using DiscordUtils;
+using System.Threading.Tasks;
+
+namespace MangasFan_Bot
+{
+    public class CommunicationModule : ModuleBase
+    {
+        [Command("Info")]
+        public async Task Info()
+        {
+            await ReplyAsync("", false, Utils.GetBotInfo(Program.P.StartTime, "MangasFan-Bot", Program.P.client.CurrentUser));
+        }
+    }
+}
---
+++ MangasFan-Bot/Program.cs
@@ -0,0 +1,60 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordUtils;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MangasFan_Bot
+{
+    class Program
+    {
+        public static async Task Main()
+            => await new Program().MainAsync();
+
+        public readonly DiscordSocketClient client;
+        private readonly CommandService commands = new CommandService();
+
+        public DateTime StartTime { private set; get; }
+        public static Program P { private set; get; }
+
+        private Program()
+        {
+            P = this;
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+            });
+            client.Log += Utils.Log;
+            commands.Log += Utils.LogError;
+        }
+
+        private async Task MainAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+
+            await commands.AddModuleAsync<CommunicationModule>(null);
+
+            if (!File.Exists("Keys/token.txt"))
+                throw new FileNotFoundException("Missing token.txt in Keys/");
+            await client.LoginAsync(TokenType.Bot, File.ReadAllText("Keys/token.txt"));
+            StartTime = DateTime.Now;
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            if (msg == null || arg.Author.IsBot) return;
+            int pos = 0;
+            if (msg.HasMentionPrefix(client.CurrentUser, ref pos) || msg.HasStringPrefix("m.", ref pos))
+            {
+                SocketCommandContext context = new SocketCommandContext(client, msg);
+                await commands.ExecuteAsync(context, pos, null);
+            }
+        }
+    }
+}
